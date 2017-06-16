aa0f3b3df74fe1085a283141585ec811a52f3cc2
sahbia <ouaja.sahbia@gmail.com>
2017-06-16 16:15:22 +0200

    style card section

--- js/main.js
+++ js/main.js
@@ -10,3 +10,36 @@ function affichmenudrop()
   }
   
 }
+// img.onmouseover = function(){affichalt(this)};
+// img.onmouseover = function(){cacheralt(this)};
+function affichalt(x)
+{
+
+      
+      var z = document.getElementsByClassName("alttexte");
+      var c= document.getElementsByClassName("entet-card");
+      for(i=0; i<c.length;i++)
+      {
+         c[i].style.position="relative";
+      }
+      for(i=0; i<z.length;i++)
+      {
+          z[i].innerHTML =x.alt;
+         z[i].style.position="absolute";
+      // z.style.border="1px solid red";
+         z[i].style.textAlign="center";
+         z[i].style.paddingTop="20px";
+         z[i].style.color="#ffa552";
+         z[i].style.width="200px";
+         z[i].style.height="80px";
+         z[i].style.backgroundColor="rgba(0,0,0,0.30)";
+         z[i].style.top="30%";
+         z[i].style.left="30%";  
+      }   
+     
+    
+}
+// function cacheralt(x)
+// {
+//    document.getElementById("alttexte").style.display="none"; 
+//  }
