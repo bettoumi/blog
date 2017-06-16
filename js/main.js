37d8418d071a9344cc9e70f69628a7637fda35f4
sahbia <ouaja.sahbia@gmail.com>
2017-06-16 13:58:27 +0200

    add style for header

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function affichmenudrop()
+{
+  var m= document.getElementById("meunudropdown");
+  if(m.style.display==="none")
+  {
+  	m.style.display = "flex";
+  } else
+  {
+  	m.style.display = "none";
+  }
+  
+}
